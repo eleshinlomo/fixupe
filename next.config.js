34af2fa84e9f3a3e4bedcff5434e80de595ae58f
eleshinlomo <seun.olatunji2@gmsil.com>
2024-10-26 14:03:51 -0400

    feat-add csrf view

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
+// next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false,
   images: {
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
+    domains: [
+      "oaidalleapiprodscus.blob.core.windows.net",
+      // Add other domains if needed
     ],
-  },
+  }
 };
 
 module.exports = nextConfig;
